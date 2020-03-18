3c20a959fbe87d18edce1ca566d471012feb256c
MoosHU1 <moos.boot@student.hu.nl>
2020-03-18 10:12:46 +0100

    collaborative filtering werkt

---
+++ create_tables.py
@@ -0,0 +1,46 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+# Connect to PostgreSQL DBMS
+con = psycopg2.connect("dbname = voordeelshop user=postgres password='okinawa'")
+con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+# Obtain a DB Cursor
+cursor = con.cursor()
+
+
+def create_tables():
+    commands = (
+        '''
+        CREATE TABLE content_recommendations
+        (
+        product_cart_id varchar,
+        product_recommendation_id varchar,
+        FOREIGN KEY (product_cart_id) references products(id),
+        FOREIGN KEY (product_recommendation_id) references products(id)
+        )
+        ''',
+        '''
+        CREATE TABLE colab_recommendations
+        (
+        profid varchar,
+        category varchar,
+        FOREIGN KEY (profid) references profiles(id)
+        )
+        ''')
+
+    try:
+        for command in commands:
+            cursor.execute(command)
+
+        cursor.close()
+        con.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if con is not None:
+            con.close()
+
+
+create_tables()
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import psycopg2
 
 
-def get(colom, table):
+def get(colom, table, limit):
     try:
-        connection = psycopg2.connect("dbname = voordeelshop user=postgres password=''")
+        connection = psycopg2.connect("dbname = voordeelshop user=postgres password='okinawa'")
 
         cursor = connection.cursor()
-        postgreSQL_select_Query = "select "+colom+" from "+table
+        if colom == 'a':
+            postgreSQL_select_Query = "SELECT profid, category FROM profiles_previously_viewed, products WHERE prodid = id LIMIT 100;"
+
+        elif limit == '':
+            postgreSQL_select_Query = "select " +colom+" from " + table
+        else:
+            postgreSQL_select_Query = "select "+colom+" from "+table+" limit "+limit
+
 
         cursor.execute(postgreSQL_select_Query)
         print("Selecting rows from products table using cursor.fetchall")
@@ -17,27 +24,86 @@ def get(colom, table):
     except (Exception, psycopg2.Error) as error:
         print("Error while fetching data from PostgreSQL", error)
 
-    finally:
-        # closing database connection.
-        if (connection):
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
+
+def insert_into_postgres(table, values):
+    try:
+        connection = psycopg2.connect("dbname = voordeelshop user=postgres password='okinawa'")
+        cursor = connection.cursor()
+
+        if table == "content_recommendations":
+            cursor.execute("""INSERT INTO content_recommendations VALUES({},{})""".format(values[0], values[1]))
+        elif table == "colab_recommendations":
+            cursor.execute("""INSERT INTO colab_recommendations VALUES(%s,%s)""",(values[0], values[1]))
+
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "Record inserted successfully into table")
+
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to insert record into table", error)
 
 
 def content_filtering():
-    records = get("*", "products")
+    records = get("*", "products",'')
+
+    #   Hardcoded voor deze opdracht
+    productid_in_cart = '45416'
     category_in_cart = 'Huishouden'
     target_in_cart = 'Vrouwen'
     sellingprice_in_cart = 190
+
+
+    recommended_ids = []
+
     for row in records:
         if row[4] == category_in_cart and \
                 sellingprice_in_cart-20 < row[10] < sellingprice_in_cart+20 and \
                 row[7] == target_in_cart:
 
-            print(row[1])
+            recommended_ids.append(row[0])
 
-def collaborative_filtering():
+    for recommendation in recommended_ids:
+        insert_into_postgres("content_recommendations", (productid, recommendation))
+
+
+
+
+def collaborative_filtering():  # Profielen koppelen aan categorie
+    profiles_category_get = get("a", "", "")
+
+    profiles_category = []
+    for item in profiles_category_get:
+        if profiles_category_get.count(item) > 2:
+            profiles_category.append(item)
+
+    profiles_category = set(profiles_category)
+
+    for item in profiles_category:
+        print(item)
+        insert_into_postgres("colab_recommendations", (item))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def collaborative_filtering_test():
     records = get("*", "profiles_previously_viewed")
     productids_cart = ['8510', '20371']
     profids = []
@@ -52,5 +118,20 @@ def collaborative_filtering():
                 profids.append(row[0])
 
 
-    print(prodids)
+def collaborative_filtering_test_(): #    Profielen koppelen aan categorie
+    profiles_viewed_category_get = get("a", "", "")
+
+    #   Deze 3 for loops halen alle profielen weg die minder dan 3 producten hebben bekeken
+    profids = []
+    for item in profiles_viewed_category_get:
+        profids.append(item[0])
+
+    for item in profids:
+        if profids.count(item) <=3:
+            profids.remove(item)
+
+    profiles_viewed_category = []
+    for item in profiles_viewed_category_get:
+        if item[0] in profids:
+            profiles_viewed_category.append(item)
 
